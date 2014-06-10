d77b9b059baa3820c321e2bf26ae2096c41286f9
Terence Sun <tsun@lbl.gov>
2014-06-10 11:07:42 -0700

    Fixed getting images from /account/.../photo/

--- account/adapters/nersc_adapter.py
+++ account/adapters/nersc_adapter.py
@@ -1,6 +1,6 @@
 import requests
 from django.conf import settings
-from django.http import Http404, HttpResponseServerError
+from django.http import Http404, HttpResponseServerError, HttpResponse
 from common.response import json_response
 
 nim_base_url = settings.NEWT_CONFIG['NIM_BASE_URL']
@@ -20,11 +20,14 @@ def get_usage(path):
         return json_response(status="ERROR", status_code=500, error="Invalid resource location: " + path)
 
 def get_image(query):
+    import re
     r = requests.get(nim_base_url + "/info/json/image/" + query)
     if r.status_code == 200:
+        if re.search(r'\/photo\/?$', query):
+            return HttpResponse(r.content, content_type=r.headers['content-type'])
         return r.json()['items']
     elif r.status_code == 404:
-        json_response(status="ERROR", status_code=404, error="Image not found at: " + path)
+        return json_response(status="ERROR", status_code=404, error="Image not found at: " + path)
     else:
         return json_response(status="ERROR", status_code=500, error="Invalid image location: " + query)
 
