92edf3a8ce8a42ae547cd7d573fa7a433ca5f42b
Terence Sun <tsun@lbl.gov>
2014-06-12 14:03:50 -0700

    Completed template and created unix_adapter

--- job/adapters/unix_adapter.py
+++ job/adapters/unix_adapter.py
@@ -1,16 +1,3 @@
-"""Job Adapter Template File
-
-IMPORTANT: NOT A FUNCTIONAL ADAPTER. FUNCTIONS MUST BE IMPLEMENTED
-
-Notes:
-    - Each of the functions defined below must return a json serializable
-      object, json_response, or valid HttpResponse object
-    - A json_response creates an HttpResponse object given parameters:
-        - content: string with the contents of the response 
-        - status: string with the status of the response 
-        - status_code: HTTP status code 
-        - error: string with the error message if there is one 
-"""
 from common.response import json_response
 import logging
 logger = logging.getLogger(__name__)
