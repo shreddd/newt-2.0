dbbbf6ba606c09e877fe8b794fb6142b798d7e64
Terence Sun <tsun@lbl.gov>
2014-06-16 13:03:00 -0700

    Added comments to emulate_job_run.sh

--- job/adapters/emulate_job_run.sh
+++ job/adapters/emulate_job_run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Usage: echo "sleep 5; ls" | ./test.sh hi.out
 
+
+# Creates (if does not exist) the processes folder
 process_dir="/tmp/newt_processes"
 mkdir -p $process_dir
 if [ ! -d "$process_dir" ]; then
@@ -8,11 +10,14 @@ if [ ! -d "$process_dir" ]; then
     exit 126
 fi
 
+# Get command and tempfile from input
 tmpfile=$1
 cmd=$2
 
-# Run the command
+# Creates the tmpfile
 touch "/tmp/newt_processes/$tmpfile"
+
+# Run the command
 eval "$cmd > /tmp/newt_processes/$tmpfile"&
 
 process_pid=$!
@@ -22,6 +27,7 @@ time_start=`date -u +%s`
 
 while ps -p $process_pid >> /dev/null
 do
+    # Print the status to the log file as long as the PID exists in the ps command
     echo "$process_pid; 999; $time_start; " > "/tmp/newt_processes/$process_pid.log"
     sleep 3
 done
@@ -29,5 +35,9 @@ done
 wait $process_pid
 my_status=$?
 time_end=`date -u +%s`
+
+# Print the completed status to the log file
 echo "$process_pid; $my_status; $time_start; $time_end" > "/tmp/newt_processes/$process_pid.log"
+
+# Append the data from the command output (to the log file)
 cat "/tmp/newt_processes/$tmpfile" >> "/tmp/newt_processes/$process_pid.log"
