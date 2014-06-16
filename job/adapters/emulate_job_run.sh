59a266f3b729927d8b5793b7a4d19b551d0968ab
Terence Sun <tsun@lbl.gov>
2014-06-16 10:55:40 -0700

    Added bash script to emulate a job run

---
+++ job/adapters/emulate_job_run.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# Usage: echo "sleep 5; ls" | ./test.sh hi.out
+
+process_dir="/tmp/newt_processes"
+mkdir -p $process_dir
+if [ ! -d "$process_dir" ]; then
+    echo "The process folder could not be created!"
+    exit 126
+fi
+
+tmpfile=$1
+cmd=$2
+
+# Run the command
+eval "$cmd > /tmp/newt_processes/$tmpfile"&
+
+process_pid=$!
+
+echo $process_pid
+time_start=`date -u +%s`
+
+while ps -p $process_pid >> /dev/null
+do
+    echo "$process_pid; 999; $time_start; " > "/tmp/newt_processes/$process_pid.log"
+    # Place started timestamp
+    sleep 3
+done
+
+wait $process_pid
+my_status=$?
+time_end=`date -u +%s`
+echo "$process_pid; $my_status; $time_start; $time_end" > "/tmp/newt_processes/$process_pid.log"
+cat "/tmp/newt_processes/$tmpfile" >> "/tmp/newt_processes/$process_pid.log"
