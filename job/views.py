ce298566efdf7922eb466ad03f6c0dd74d54e99e
Terence Sun <tsun@lbl.gov>
2014-06-16 12:45:41 -0700

    Created job view and urls

--- job/views.py
+++ job/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from newt.views import JSONRestView
+from common.response import json_response
+from django.conf import settings
+import urllib
 
-# Create your views here.
+job_adapter = __import__(settings.NEWT_CONFIG['ADAPTERS']['JOB'], globals(), locals(), ['adapter'], -1)
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+class JobRootView(JSONRestView):
+    def get(self, request):
+        return job_adapter.get_queues()
+
+class JobQueueView(JSONRestView):
+    def get(self, request, queue):
+        return job_adapter.view_queue(request, queue)
+    def post(self, request, queue):
+        return job_adapter.submit_job(request, queue)
+
+class JobDetailView(JSONRestView):
+    def get(self, request, queue, job_id):
+        return job_adapter.get_info(queue, job_id)
+
+    def delete(self, request, queue, job_id):
+        return job_adapter.delete_job(queue, job_id)
