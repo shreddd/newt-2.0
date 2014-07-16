ad40b27d1e73abfa7fdf05dfd8db738c5ff60b90
Terence Sun <tsun@lbl.gov>
2014-07-16 10:11:06 -0700

    Changed tests for job

--- job/tests.py
+++ job/tests.py
@@ -5,6 +5,10 @@ import time
 import os
 from newt.tests import MyTestClient, newt_base_url, login
 
+try:
+    from newt.local_settings import test_machine as machine
+except ImportError:
+    machine = "localhost"
 
 class JobTests(TestCase):
     fixtures = ["test_fixture.json"]
@@ -14,51 +18,57 @@ class JobTests(TestCase):
         self.client = MyTestClient()
         self.client.post(newt_base_url + "/auth", data=login)
 
-    def test_running_cmds(self):
+    def test_get_queues(self):
         # Tests getting queues
         r = self.client.get(newt_base_url + "/job/")
         self.assertEquals(r.status_code, 200)
+        json_response = r.json()
+        self.assertTrue(len(json_response['output']) > 0)
+        self.assertIn(machine, json_response['output'].keys())
 
+    def test_running_cmds(self):
         # Tests submitting a job
         payload = {
-            "jobscript": "sleep 5\nls ~"
+            "jobscript": "sleep 5"
         }
-        r = self.client.post(newt_base_url + "/job/localhost/", data=payload)
+        r = self.client.post(newt_base_url + "/job/"+machine+"/", data=payload)
         self.assertEquals(r.status_code, 200)
         json_response = r.json()
         self.assertIsNot(json_response['output']['jobid'], None)
+
+        # Get job id from submitting the job
         job_id = json_response['output']['jobid']
 
         # Tests getting job info
-        r = self.client.get(newt_base_url + "/job/localhost/%s/" % job_id)
+        r = self.client.get(newt_base_url + "/job/"+machine+"/%s/" % job_id)
         self.assertEquals(r.status_code, 200)
         json_response = r.json()
         self.assertEquals(json_response['output']['jobid'], job_id)
         self.assertEquals(json_response['output']['user'], login['username'])
         
         # Delete job from queue
-        r = self.client.delete(newt_base_url + "/job/localhost/%s/" % job_id)
+        r = self.client.delete(newt_base_url + "/job/"+machine+"/%s/" % job_id)
         self.assertEquals(r.status_code, 200)
 
-    def test_short_cmd(self):
-        payload = {
-            "jobscript": "ls -a /"
-        }
-        r = self.client.post(newt_base_url + "/job/localhost/", data=payload)
-        self.assertEquals(r.status_code, 200)
-        json_response = r.json()
-        self.assertIsNot(json_response['output']['jobid'], None)
-        job_id = json_response['output']['jobid']
+    # def test_short_cmd(self):
+    #     payload = {
+    #         "jobscript": "ls -a /"
+    #     }
+    #     r = self.client.post(newt_base_url + "/job/"+machine+"/", data=payload)
+    #     self.assertEquals(r.status_code, 200)
+    #     json_response = r.json()
+    #     self.assertIsNot(json_response['output']['jobid'], None)
+    #     job_id = json_response['output']['jobid']
 
-        time.sleep(1)
+    #     time.sleep(1)
 
-        r = self.client.get(newt_base_url + "/job/localhost/%s/" % job_id)
-        self.assertEquals(r.status_code, 200)
-        json_response = r.json()
-        self.assertEquals(json_response['output']['jobid'], job_id)
-        self.assertEquals(json_response['output']['user'], login['username'])
-        self.assertEquals(json_response['output']['status'], '0')
-        files = ['.', '..'] + os.listdir("/")
-        files.sort()
-        cmd_out = json_response['output']['output'].splitlines()
-        self.assertEquals(files, cmd_out)
+    #     r = self.client.get(newt_base_url + "/job/"+machine+"/%s/" % job_id)
+    #     self.assertEquals(r.status_code, 200)
+    #     json_response = r.json()
+    #     self.assertEquals(json_response['output']['jobid'], job_id)
+    #     self.assertEquals(json_response['output']['user'], login['username'])
+    #     self.assertEquals(json_response['output']['status'], '0')
+    #     files = ['.', '..'] + os.listdir("/")
+    #     files.sort()
+    #     cmd_out = json_response['output']['output'].splitlines()
+    #     self.assertEquals(files, cmd_out)
