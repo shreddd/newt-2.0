ec8f66011cd9ce66f913a9628719951ffd333c7f
Shreyas Cholia <shreyas@gmail.com>
2016-12-15 20:52:07 +0100

    Cleanup status adapter

--- status/views.py
+++ status/views.py
@@ -1,14 +1,10 @@
 # Create your views here.
-from newt.views import JSONRestView
-from common.response import json_response
-from django.http import HttpResponseServerError
-from django.conf import settings
-
-from importlib import import_module
-adapter = import_module(settings.NEWT_CONFIG['ADAPTERS']['STATUS']['adapter'])
-
 import logging
+from newt.views import JSONRestView
+from django.conf import settings
+from importlib import import_module
 
+status_adapter = import_module(settings.NEWT_CONFIG['ADAPTERS']['STATUS']['adapter'])
 
 logger = logging.getLogger("newt." + __name__)
 
@@ -16,8 +12,9 @@ logger = logging.getLogger("newt." + __name__)
 class StatusView(JSONRestView):
     def get(self, request, machine_name=None):
         logger.debug("Entering %s:%s" % (self.__class__.__name__, __name__))
-        return adapter.get_status(machine_name)
+        return status_adapter.get_status(machine_name)
+
 
 class ExtraStatusView(JSONRestView):
     def get(self, request, query):
-        return adapter.extras_router(request, query)
+        return status_adapter.extras_router(request, query)
